0a31b9f005bb6d0baf7c92914d3c3e19239a8353
TQMatvey <77576395+TQMatvey@users.noreply.github.com>
2022-06-11 07:29:50 +0000

    updates + toolchains

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-export ARCH=arm64
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export clang-triple = android/prebuilts/clang/host/linux-x86/clang-4639204/bin/aarch64-linux-gnu-
-export ANDROID_MAJOR_VERSION=s
+# Variables
+#DIR=`readlink -f .`;
+#PARENT_DIR=`readlink -f ${DIR}/..`;
+
 export PLATFORM_VERSION=12
+export ANDROID_MAJOR_VERSION=s
+export CROSS_COMPILE=toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CC=toolchains/clang-4639204/bin/clang
+export CLANG_TRIPLE=toolchains/clang-4639204/bin/aarch64-linux-gnu-
+export ARCH=arm64
+KERNEL_MAKE_ENV="LOCALVERSION=-TQMatvey"
+
+#export ARCH=arm64
+#export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+#export clang-triple = android/prebuilts/clang/host/linux-x86/clang-4639204/bin/aarch64-linux-gnu-
+#export ANDROID_MAJOR_VERSION=s
+#export PLATFORM_VERSION=12
 
 make exynos9610-m31snsxx_defconfig
-make -j64
+make -j$(nproc)
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.14.113-gbe9fff621-dirty"
